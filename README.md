94db5f74ba066533607b17d8503230c57bd5d8cd
Rocky <542622608@qq.com>
2016-02-23 11:41:11 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# VideoRecord
+# VideoRecord
+# VideoRecord
+# VideoRecord
---
+++ VideoRecord/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoRecord
+//
+//  Created by ZCBL on 16/2/23.
+//  Copyright © 2016年 ZCBL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
